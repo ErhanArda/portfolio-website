af08feca67289fa1ea691b505d48e5bc3c149b31
Erhan Arda <erhanarda7@gmail.com>
2020-05-30 12:54:29 +0300

    Updates

---
+++ precache-manifest.acdfe18471c8c21fb3b3ef04e55161fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea89cde7cc0328a3a70fb1284735349",
+    "url": "/ErhanArda/portfolio-website/index.html"
+  },
+  {
+    "revision": "9c6c17a4fa1a24367b24",
+    "url": "/ErhanArda/portfolio-website/static/css/2.fb20f2ef.chunk.css"
+  },
+  {
+    "revision": "b150f989b5604d6c620c",
+    "url": "/ErhanArda/portfolio-website/static/css/main.a51f08d5.chunk.css"
+  },
+  {
+    "revision": "9c6c17a4fa1a24367b24",
+    "url": "/ErhanArda/portfolio-website/static/js/2.ba263c43.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/ErhanArda/portfolio-website/static/js/2.ba263c43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b150f989b5604d6c620c",
+    "url": "/ErhanArda/portfolio-website/static/js/main.df2eae4d.chunk.js"
+  },
+  {
+    "revision": "1ca386a33a779fe562ff",
+    "url": "/ErhanArda/portfolio-website/static/js/runtime-main.24bc5204.js"
+  }
+]);
